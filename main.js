c56fb5db73b33105d68363668f313d28b95e8ccc
Nnaemeka Ndaguba <ndagubannaemeka@gmail.com>
2021-02-23 19:21:45 -0600

    Update main.js

--- main.js
+++ main.js
@@ -11,10 +11,3 @@ $('#navigation').slideToggle(700);
 
     })
 });
-
-newArray = [];
-newArray = [8,7,6,5,4,3,2,1];
-console.log(newArray[0] + newArray[3]);
-console.log(newArray.sort());
-newArray.push(9);
-console.log(newArray);
